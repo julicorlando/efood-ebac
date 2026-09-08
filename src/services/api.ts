59c9d074d3008bae18f07d42e17a550773f5331d
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-08 14:16:33 -0300

    feat: adiciona API de checkout com RTK Query

---
+++ src/services/api.ts
@@ -0,0 +1,51 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export type PurchasePayload = {
+  products: Array<{
+    id: number
+    price: number
+  }>
+  delivery: {
+    receiver: string
+    address: {
+      description: string
+      city: string
+      zipCode: string
+      number: number
+      complement: string
+    }
+  }
+  payment: {
+    card: {
+      name: string
+      number: string
+      code: number
+      expires: {
+        month: number
+        year: number
+      }
+    }
+  }
+}
+
+export type PurchaseResponse = {
+  orderId: string
+}
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api-ebac.vercel.app/api/efood/'
+  }),
+  endpoints: (builder) => ({
+    purchase: builder.mutation<PurchaseResponse, PurchasePayload>({
+      query: (body) => ({
+        url: 'checkout',
+        method: 'POST',
+        body
+      })
+    })
+  })
+})
+
+export const { usePurchaseMutation } = api
