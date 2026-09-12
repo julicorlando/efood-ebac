393ac568bbe8105ebb5adb938eba373d0d523aa3
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-11 21:43:46 -0300

    fix: refina proporções da marca eFood

--- src/styles/shared.ts
+++ src/styles/shared.ts
@@ -5,6 +5,38 @@ export const Container = styled.div`
   margin: 0 auto;
 `
 
+export const Brand = styled.span`
+  position: relative;
+  display: inline-flex;
+  width: 125px;
+  height: 58px;
+  align-items: center;
+  justify-content: center;
+  color: #e66767;
+  font-family: 'Roboto', Arial, Helvetica, sans-serif;
+  font-size: 40px;
+  line-height: 1;
+  font-weight: 900;
+  letter-spacing: -3.6px;
+
+  span {
+    transform: translate(-5px, -1px);
+  }
+
+  small {
+    position: absolute;
+    top: 12px;
+    right: 4px;
+    color: #e66767;
+    font-size: 8px;
+    line-height: 9px;
+    letter-spacing: 0.8px;
+    font-weight: 900;
+    writing-mode: vertical-rl;
+    text-orientation: mixed;
+  }
+`
+
 export const Button = styled.button`
   height: 24px;
   border: 0;
