8abe48626ebe500ae249a17f38fbfec8c933d49f
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-11 21:40:36 -0300

    fix: ajusta card de restaurante ao Figma

--- src/components/RestaurantCard/index.tsx
+++ src/components/RestaurantCard/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import type { Restaurant } from '../../data/restaurants'
+import star from '../../assets/star.svg'
 import {
   Card,
   CardBody,
@@ -21,7 +22,7 @@ const RestaurantCard = ({ restaurant }: Props) => (
     <div>
       <Image src={restaurant.cover} alt={restaurant.title} />
       <Tags>
-        {restaurant.featured && <Tag>Destaque da semana</Tag>}
+        {restaurant.featured && <Tag $featured>Destaque da semana</Tag>}
         <Tag>{restaurant.type}</Tag>
       </Tags>
     </div>
@@ -30,7 +31,8 @@ const RestaurantCard = ({ restaurant }: Props) => (
       <Info>
         <Title>{restaurant.title}</Title>
         <Rating>
-          {restaurant.rating.toFixed(1)} <span aria-hidden="true">★</span>
+          {restaurant.rating.toFixed(1)}
+          <img src={star} alt="" aria-hidden="true" />
         </Rating>
       </Info>
 
